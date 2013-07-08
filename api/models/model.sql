5747a7e819a80c2acf1696e220370e126bab094d
JR Hull <companygateways@gmail.com>
2013-07-08 13:38:00 -0700

    Updated Sql Scripts

--- api/models/model.sql
+++ api/models/model.sql
@@ -5,14 +5,40 @@ CREATE TABLE `forms` (
   `api_key` varchar(45) COLLATE latin1_general_ci DEFAULT NULL,
   `tags` varchar(45) COLLATE latin1_general_ci DEFAULT NULL,
   `title` varchar(45) COLLATE latin1_general_ci DEFAULT NULL,
-  `description` varchar(1024) COLLATE latin1_general_ci DEFAULT NULL,
+  `description` varchar(1024) COLLATE latin1_general_ci NOT NULL,
   `meta` text COLLATE latin1_general_ci,
-  `date_created` date DEFAULT NULL,
-  `date_modified` varchar(45) COLLATE latin1_general_ci DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `date_modified` datetime DEFAULT NULL,
+  `is_public` tinyint(4) DEFAULT '0',
+  `is_published` tinyint(4) DEFAULT '0',
+  `is_deleted` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci COMMENT='Open Report Form'$$
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci COMMENT='Open Report Form'$$
 
 delimiter $$
+
+CREATE TABLE `assignments` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `api_key` varchar(32) COLLATE latin1_general_ci DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `form_id` bigint(20) DEFAULT NULL,
+  `date_assigned` datetime DEFAULT NULL,
+  `is_active` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci$$
+
+delimiter $$
+
+CREATE TABLE `reports` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `api_key` varchar(36) COLLATE latin1_general_ci DEFAULT NULL,
+  `form_id` bigint(20) DEFAULT NULL,
+  `title` varchar(45) COLLATE latin1_general_ci DEFAULT NULL,
+  `meta` text COLLATE latin1_general_ci,
+  `is_active` tinyint(4) DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci$$
+
 
 CREATE TABLE `records` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -28,4 +54,3 @@ CREATE TABLE `records` (
   `identity` varchar(128) COLLATE latin1_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci$$
-
--- models/model.sql
+++ models/model.sql
@@ -3,20 +3,23 @@ delimiter $$
 CREATE TABLE `accounts` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `admin_id` bigint(20) DEFAULT NULL COMMENT 'user id of administrator',
-  `name` varchar(64) DEFAULT NULL,
-  `isactive` tinyint(4) DEFAULT '1',
+  `name` varchar(45) DEFAULT NULL,
+  `is_active` tinyint(4) DEFAULT '1',
   `api_key` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1$$
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8$$
+
 
 delimiter $$
 
 CREATE TABLE `users` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `username` varchar(64) DEFAULT NULL,
-  `email` varchar(512) DEFAULT NULL,
-  `password` varchar(20) DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
   `account_id` bigint(20) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `date_modified` datetime DEFAULT NULL,
   `is_active` tinyint(4) DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=103 DEFAULT CHARSET=utf8$$
+) ENGINE=InnoDB AUTO_INCREMENT=127 DEFAULT CHARSET=utf8$$
